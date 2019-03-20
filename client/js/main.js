9e834882b9b45f21a38042224894ac66f6126b0b
Erik EImterbäumer <e.eimterbaeumer@ufl.edu>
2019-03-19 21:07:37 -0400

    upload structure

---
+++ client/js/main.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    console.log('main.js');
+    
+    import Cropper from 'cropperjs';
+
+    const image = document.getElementById('image');
+    const cropper = new Cropper(image, {
+      aspectRatio: 16 / 9,
+      crop(event) {
+        console.log(event.detail.x);
+        console.log(event.detail.y);
+        console.log(event.detail.width);
+        console.log(event.detail.height);
+        console.log(event.detail.rotate);
+        console.log(event.detail.scaleX);
+        console.log(event.detail.scaleY);
+      },
+    });
+}
+ 
