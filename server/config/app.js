b052943170075ea14e6aeded94b629376071e6cb
Caleb Reed <calebreed@ufl.edu>
2019-03-26 23:35:36 -0400

    Adding firebase storage functionality back, not currently working with crop image

--- server/config/app.js
+++ server/config/app.js
@@ -1,6 +1,15 @@
-var config = require('./config'), 
-    mongoose = require('mongoose'),   
+var config = require('./config'),
+    mongoose = require('mongoose'),
+    firebase = require("firebase"),
+    FBconfig = {
+        apiKey: "AIzaSyC8iqW-J1UCgSg7GHez4SuO51APq_n6AJI",
+        authDomain: "cen3031-photography-group3.firebaseapp.com",
+        databaseURL: "https://cen3031-photography-group3.firebaseio.com",
+        storageBucket: "cen3031-photography-group3.appspot.com"
+    },
     express = require('./express');
+
+firebase.initializeApp(FBconfig);
 
 module.exports.start = function() {
   var app = express.init();
